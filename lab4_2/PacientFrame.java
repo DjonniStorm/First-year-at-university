d4b348ba9cc41b20f481967c5bd2383f85ce8772
DjonniStorm <48327702+DjonniStorm@users.noreply.github.com>
2024-04-26 17:20:44 +0400

    complete

--- lab4_2/PacientFrame.java
+++ lab4_2/PacientFrame.java
@@ -24,6 +24,7 @@ public class PacientFrame extends JFrame {
 
         JButton buttonAdd = new JButton("Добавить");
         JButton buttonDel = new JButton("Удалить");
+        JButton buttonRemake = new JButton("Изменить");
 
         GridLayout iMoreLikeFlexbox = new GridLayout(0, 1);
 
@@ -44,6 +45,7 @@ public class PacientFrame extends JFrame {
         leftBlock.add(yearOfBirthField);
         leftBlock.add(buttonAdd);
         leftBlock.add(buttonDel);
+        leftBlock.add(buttonRemake);
 
         defaultListModel = new DefaultListModel<>();
         list = new JList<>(defaultListModel);
@@ -108,6 +110,68 @@ public class PacientFrame extends JFrame {
                 for (int i = 0; i < a.length; i++) {
                     defaultListModel.addElement(a[i]);
                 }
+            }
+        });
+
+        buttonRemake.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (list.getSelectedValue() != null) {
+                    JFrame frame = new JFrame("Изменение параметров");
+                    frame.setDefaultLookAndFeelDecorated(true);
+                    frame.setVisible(true);
+                    JPanel panel = new JPanel();
+                    frame.pack();
+                    frame.setContentPane(panel);
+                    frame.setSize(500, 300);
+                    frame.setLocation(200, 200);
+
+                    JTextField nameField        = new JTextField();
+                    JTextField surnameField     = new JTextField();
+                    JTextField yearOfBirthField = new JTextField();
+                    JTextField wasLike          = new JTextField(list.getSelectedValue().toString());
+
+                    JLabel about     = new JLabel("Изменить параметры", SwingConstants.CENTER);
+                    JLabel was       = new JLabel("Вот так было:", SwingConstants.CENTER);
+                    JLabel name      = new JLabel("Имя:", SwingConstants.CENTER);
+                    JLabel surname   = new JLabel("Фамилия:", SwingConstants.CENTER);
+                    JLabel yearOfB   = new JLabel("Год рождения:", SwingConstants.CENTER);
+                    panel.setLayout(new GridLayout(0, 1));
+                    panel.add(about);
+                    panel.add(was);
+                    panel.add(wasLike);
+                    panel.add(name);
+                    panel.add(nameField);
+                    panel.add(surname);
+                    panel.add(surnameField);
+                    panel.add(yearOfB);
+                    panel.add(yearOfBirthField);
+                    JButton button = new JButton("Готово");
+                    button.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent actionEvent) {
+                            try {
+
+                                listP.patientData.add(listP.patientData.indexOf(list.getSelectedValue()), new Pacient(Integer.parseInt(yearOfBirthField.getText()), nameField.getText(), surnameField.getText()));
+                                listP.patientData.remove(list.getSelectedValue());
+                                defaultListModel.clear();
+                                Object[] a = listP.to_String();
+                                for (int i = 0; i < a.length; i++) {
+                                    defaultListModel.addElement(a[i]);
+                                }
+                            } catch (Exception ex) {
+                                JOptionPane.showMessageDialog(null, "Неправильные данные ");
+                            } finally {
+                                frame.dispose();
+                            }
+
+                        }
+                    });
+                    panel.add(button);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Не выбрано");
+                }
+
             }
         });
     }
